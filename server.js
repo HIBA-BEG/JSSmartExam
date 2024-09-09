7f2b3447ffcd325045434302e62ba3e4eb66bc93
mohammed-rharbi <rharbi383@gmail.com>
2024-09-09 10:23:11 +0100

    add : create triner components

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
+const port = 3000;
 
-app.get("/", function(req, res) {
-    return res.send("Hello World");
-});
+// Middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap',express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+
+app.get('/' ,(req, res)=>{
+   return  res.render('index');
+})
+
+app.get('/home' ,(req, res)=>{
+  return  res.render('./home');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
